6e1c9fab86954609572443adaa69ce37c7c50c57
Wanyu Lee <wanyulee1998@gmail.com>
2023-09-06 13:05:42 +0800

    Adjust some bug in Request 2, and complete Request 3

--- week-2/Assignment-4/all.js
+++ week-2/Assignment-4/all.js
@@ -2,6 +2,9 @@ const h1 = document.querySelector('h1[class=banner-title]');
 const mobileMenuBtn = document.getElementById('mobileMenuBtn');
 const headerMenu = document.getElementById('headerMenu');
 const quitMenuBtn = document.getElementById('quitMenuBtn');
+const articleMore = document.getElementById('articleMore');
+const readMoreBtn = document.getElementById('readMoreBtn');
+
 
 // Request 1: Click to Change Text
 h1.addEventListener('click', () => {
@@ -14,13 +17,34 @@ h1.addEventListener('click', () => {
 
 
 // Request 2: Click to Show/Close Menu
-mobileMenuBtn.addEventListener('click', () => {
+mobileMenuBtn.addEventListener('click', () => { // Click to show menu
     headerMenu.style.display = "flex";
     quitMenuBtn.style.display = "flex";
 
 })
 
-quitMenuBtn.addEventListener('click', () => {
+quitMenuBtn.addEventListener('click', () => {   // Click to close menu
     headerMenu.removeAttribute('style');
     quitMenuBtn.removeAttribute('style');
+})
+
+window.addEventListener('resize', (e) => {      // Resize window close menu
+    if (window.innerWidth > 800) {
+        headerMenu.removeAttribute('style');
+        quitMenuBtn.removeAttribute('style');
+        return;
+    }
+})
+
+
+// Request 3: Click to Show More Content Boxes
+readMoreBtn.addEventListener('click', () => {
+    if (readMoreBtn.textContent === '閱讀更多') {
+        articleMore.style.display = "flex";
+        articleMore.scrollIntoView();
+        readMoreBtn.textContent = "收起文章"
+    } else {
+        articleMore.removeAttribute('style');
+        readMoreBtn.textContent = "閱讀更多"
+    }
 })
