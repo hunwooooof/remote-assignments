fdc5ce194e3e819e02a1c2cb3af2bb7b475f7181
Wanyu Lee <wanyulee1998@gmail.com>
2023-09-06 12:06:11 +0800

    Complete Request 2: Click to show and close menu

--- week-2/Assignment-4/all.js
+++ week-2/Assignment-4/all.js
@@ -1,7 +1,9 @@
 const h1 = document.querySelector('h1[class=banner-title]');
+const mobileMenuBtn = document.getElementById('mobileMenuBtn');
+const headerMenu = document.getElementById('headerMenu');
+const quitMenuBtn = document.getElementById('quitMenuBtn');
 
-
-// Request 1: Click to Change Text.
+// Request 1: Click to Change Text
 h1.addEventListener('click', () => {
     if (h1.textContent === "It's Her Shot!") {
         h1.textContent = "Have a Good Time!";
@@ -9,3 +11,16 @@ h1.addEventListener('click', () => {
         h1.textContent = "It's Her Shot!";
     }
 });
+
+
+// Request 2: Click to Show/Close Menu
+mobileMenuBtn.addEventListener('click', () => {
+    headerMenu.style.display = "flex";
+    quitMenuBtn.style.display = "flex";
+
+})
+
+quitMenuBtn.addEventListener('click', () => {
+    headerMenu.removeAttribute('style');
+    quitMenuBtn.removeAttribute('style');
+})
