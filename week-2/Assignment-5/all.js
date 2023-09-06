447fc13b76d6344a24ecfaac0dc31f2c2da881eb
Wanyu Lee <wanyulee1998@gmail.com>
2023-09-06 14:37:53 +0800

    Complete assignment-5

---
+++ week-2/Assignment-5/all.js
@@ -0,0 +1,36 @@
+/**
+ * [Give an array of integers to the function and it'll return indices of the two numbers such that they add up to a specific target. Assume that each input would have exactly one solution, and can't use the same element twice.]
+ *
+ * @param {array} nums - [An array of integers which includes two answers that can add up to the specific target]
+ * @param {number} target - [Which is the target that two numbers add up tp]
+ * @returns {array} [Includes the indices of the two numbers]
+ */
+
+
+
+function twoSum(nums, target) {
+    let output = [];    // Declare 'output' as answer
+    let n1;
+    let n2;             // output = [n1, n2]
+
+    // Subtract each element from the target
+    let sub;
+    for  (let i = 0; i < nums.length; i++) {
+        sub = target - nums[i];
+        if (sub > 0) {  // There's a chance that one of the other elements equals to 'sub' 
+            if (nums.indexOf(sub) !== -1) { // Make sure 'sub' is inside the nums
+                n1 = nums[i];
+                n2 = sub;
+                output.push(nums.indexOf(n1), nums.indexOf(n2));
+                break;
+            }
+        }
+    }
+    console.log(output);
+}
+
+
+twoSum([2, 7, 11, 15], 9);                  // output: [0, 1]
+twoSum([2, 7, 11, 15], 22);                 // output: [1, 3]
+twoSum([2, 7, 11, 15, 1, 100, 55], 155);    // output: [5, 6]
+twoSum([3, 7, 11, 15, 1, 100, 345], 4);     // output: [0, 4]
