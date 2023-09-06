ec4ff12d8b663086287757cc4a6eb61d4912c5e3
Wanyu Lee <wanyulee1998@gmail.com>
2023-09-06 10:10:08 +0800

    Add and complete assignment-2 in week-2

---
+++ week-2/Assignment-2/all.js
@@ -0,0 +1,28 @@
+/**
+ * [A function that calculates two numbers inside the passing object only addition and subtraction.]
+ *
+ * @param {object} args - [Includes two numbers and a plus or minus sign]
+ * @returns {number} [The max result of the calculation]
+ */
+
+
+
+function calculate(args) {
+    if (args.op === '+') {
+        let output = args.n1 + args.n2;
+        return output;
+
+    } else if (args.op === '-') {
+        let output = args.n1 - args.n2;
+        return output;
+
+    } else {
+        console.error('Not supported');
+        
+    }
+}
+
+console.log(calculate({ n1: 2, n2: 3, op: '+' })); // expected output: 5
+console.log(calculate({ n1: 5, n2: 2, op: '-' })); // expected output: 3
+console.log(calculate({ n1: 1, n2: 6, op: 'x' })); // expected output: 'Not supported'
+
