195e3f68738e4b809a8d9557d1d4bd13973696b2
Wanyu Lee <wanyulee1998@gmail.com>
2023-09-06 10:27:51 +0800

    Add and complete assignment-3 in week-2

---
+++ week-2/Assignment-3/all.js
@@ -0,0 +1,39 @@
+/**
+ * [A function that calculates the total price of all products after applying a discount.]
+ *
+ * @param {object} data - [Includes the amount of discount and infos of products include name and price]
+ * @returns {number} [The total price of all products after applying a discount]
+ */
+
+
+
+function calculate(data) {
+    // 1. Calculate the total price
+    let totalPrice = 0;
+    for (let i = 0; i < data.products.length; i++) {
+        totalPrice += data.products[i].price;
+    }
+
+    // 2. Apply the discount
+    return totalPrice * data.discount;
+}
+
+const discountedPrice = calculate({
+    discount: 0.1,
+    products: [
+        {
+            name: "Product 1",
+            price: 100
+        },
+        {
+            name: "Product 2",
+            price: 700
+        },
+        {
+            name: "Product 3",
+            price: 250
+        }
+    ]
+});
+
+console.log(discountedPrice)
