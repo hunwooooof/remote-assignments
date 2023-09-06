74a9f1eb2c251d2da2b4625da361008b35e0b64b
Wanyu Lee <wanyulee1998@gmail.com>
2023-09-06 13:09:46 +0800

    Add some semicolons

--- week-2/Assignment-4/all.js
+++ week-2/Assignment-4/all.js
@@ -20,21 +20,20 @@ h1.addEventListener('click', () => {
 mobileMenuBtn.addEventListener('click', () => { // Click to show menu
     headerMenu.style.display = "flex";
     quitMenuBtn.style.display = "flex";
-
-})
+});
 
 quitMenuBtn.addEventListener('click', () => {   // Click to close menu
     headerMenu.removeAttribute('style');
     quitMenuBtn.removeAttribute('style');
-})
+});
 
-window.addEventListener('resize', (e) => {      // Resize window close menu
+window.addEventListener('resize', () => {      // Resize window close menu
     if (window.innerWidth > 800) {
         headerMenu.removeAttribute('style');
         quitMenuBtn.removeAttribute('style');
         return;
     }
-})
+});
 
 
 // Request 3: Click to Show More Content Boxes
@@ -42,9 +41,9 @@ readMoreBtn.addEventListener('click', () => {
     if (readMoreBtn.textContent === '閱讀更多') {
         articleMore.style.display = "flex";
         articleMore.scrollIntoView();
-        readMoreBtn.textContent = "收起文章"
+        readMoreBtn.textContent = "收起文章";
     } else {
         articleMore.removeAttribute('style');
-        readMoreBtn.textContent = "閱讀更多"
+        readMoreBtn.textContent = "閱讀更多";
     }
-})
+});
