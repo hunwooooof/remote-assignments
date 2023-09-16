5554ebe7416c0b22aff0e4fa866d875a88c609a5
Wanyu Lee <wanyulee1998@gmail.com>
2023-09-16 19:04:28 +0800

    Complete assignment-5

--- week-3/app.js
+++ week-3/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static('public'));
 
 app.set('view engine', 'pug');  // app.set(name, value) Assigns setting name to value. You may store any value that you want, but certain names can be used to configure the behavior of the server.
@@ -35,9 +39,38 @@ app.get('/getData', (req,res) => {
         output.status = "error";
     }
     res.send(output);
-     
-
 })
+
+
+// Assignment-5
+app.get('/myName', (req,res) => {
+    const { username } = req.cookies;
+    if (username) {
+        res.locals.name = username;
+        res.render('hello');
+    } else {
+        const { name } = req.query
+        res.redirect('/trackName');
+    }
+});
+
+app.get('/trackName', (req, res) => {
+    const { name } = req.query;
+    res.cookie('username', name);
+    if ( name ) {
+        res.redirect('/myname');
+    } else {
+        res.render('getname');
+    }
+});
+
+
+app.post('/trackName', (req, res) => {
+    res.redirect(`/trackName?name=${req.body.username}`);
+});
+
+
+
 
 
 // error handling middleware
@@ -50,7 +83,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
-    res.render('error', );
+    res.render('error');
 })
 
 
@@ -59,3 +92,23 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {    // Binds and listens for connections on the specified host and port. 
     console.log('Running on localhost:3000...');
 });
+
+
+
+
+/*
+
+1. Serve a URL http://localhost:3000/myName by your server.✅
+2. When the user connects to http://localhost:3000/myName, check cookies for the
+user's name in the backend.
+a. If you can get the user's name from cookies, show it on the web page. Done.✅
+b. If you cannot get the user's name from cookies, show a HTML form including
+a text input and a button in the web page. Go to step 3.✅
+3. User can enter his name in the text input, and then click button to submit form to a
+URL http://localhost:3000/trackName?name=使用者的輸入 which should be served
+from your server, too.
+4. When user submits form to http://localhost:3000/trackName?name=使用者的輸入,
+you should get user’s name from HTTP parameter and store it in the cookies.
+5. Redirect user to http://localhost:3000/myName, user can see his name, finally. Done
+
+*/
