27517113809dd38f48366646eb7774d751d8d022
Wanyu Lee <wanyulee1998@gmail.com>
2023-09-24 13:40:51 +0800

    Correct some indent problem and using DOM in logic

--- week-4/assignment-3/src/App.jsx
+++ week-4/assignment-3/src/App.jsx
@@ -2,7 +2,6 @@ import './css-reset.css';
 import './App.css';
 import Article from './components/Article';
 import articleData from './assets/articleData'
-import articleData2 from './assets/articleData2';
 import Header from './components/Header';
 import Banner from './components/Banner';
 import ReadMore from './components/ReadMore';
@@ -10,20 +9,20 @@ import ReadMore from './components/ReadMore';
 
 function App() {
 
-  return (
-    <>
-      <Header />
-      <Banner />
+	return (
+		<>
+			<Header />
+			<Banner />
 
-      <main className="container">
-        <h2 className="main-title">女籃快報</h2>
-        <div className="col-box">
-          <Article data={articleData}/>
-        </div>
-        <ReadMore/>
-      </main>
-    </>
-  );
+			<main className="container">
+				<h2 className="main-title">女籃快報</h2>
+				<div className="col-box">
+					<Article data={articleData} />
+				</div>
+				<ReadMore />
+			</main>
+		</>
+	);
 }
 
 export default App;
--- week-4/assignment-3/src/components/ReadMore.jsx
+++ week-4/assignment-3/src/components/ReadMore.jsx
@@ -5,25 +5,25 @@ import { useState } from "react";
 
 function ReadMore() {
     const [btnReadMore, setBtnReadMore] = useState("閱讀更多");
-    const [style, setStyle] = useState({display: "none"});
+    const [style, setStyle] = useState({ display: "none" });
     const handleClickReadMore = (e) => {
-        if (e.target.innerHTML === "閱讀更多") {
+        if (btnReadMore === "閱讀更多") {
             setBtnReadMore("收起文章");
-            setStyle({display: "flex"});
+            setStyle({ display: "flex" });
         } else {
             setBtnReadMore("閱讀更多");
-            setStyle({display: "none"});
+            setStyle({ display: "none" });
         }
     };
 
     return (
         <>
-        <div className="readmore">
-          <button id="readMoreBtn" onClick={handleClickReadMore}>{btnReadMore}</button>
-        </div>
-        <div className="col-box article-more" id="articleMore" style={style}>
-          <Article data={articleData2}/>
-        </div>
+            <div className="readmore">
+                <button id="readMoreBtn" onClick={handleClickReadMore}>{btnReadMore}</button>
+            </div>
+            <div className="col-box article-more" id="articleMore" style={style}>
+                <Article data={articleData2} />
+            </div>
         </>
     )
 }
